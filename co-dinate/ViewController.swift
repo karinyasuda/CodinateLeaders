197f92536cf681579958aeb3fc9fa9f997e2e3b0
karinyasuda <rin.03k35@gmail.com>
2015-03-09 16:45:06 +0900

    fix this data

--- co-dinate/ViewController.swift
+++ co-dinate/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var Button1:UIButton!
-    @IBOutlet var Button2:UIButton!
-    @IBOutlet var Button3:UIButton!
+//    @IBOutlet var Button1:UIButton!
+//    @IBOutlet var Button2:UIButton!
+//    @IBOutlet var Button3:UIButton!
 
     
    
@@ -19,22 +19,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        let btn:UIButton? = UIButton.buttonWithType(UIButtonType.Custom) as? UIButton;
-        btn!.frame = CGRectMake(100, 0, 100, 100);
-//        btn!.setImage(img, forState: UIControlState.Normal);
-        btn!.addTarget(self, action: "onClick:", forControlEvents: UIControlEvents.TouchUpInside);
-        
-        Button1.layer.cornerRadius = 10
-        self.view.addSubview(Button1)
-        Button1.layer.borderWidth = 1
-        Button2.layer.cornerRadius = 10
-        self.view.addSubview(Button2)
-        Button2.layer.borderWidth = 1
-        Button3.layer.cornerRadius = 10
-        self.view.addSubview(Button3)
-        Button3.layer.borderWidth = 1
-        
-      
+//        let btn:UIButton? = UIButton.buttonWithType(UIButtonType.Custom) as? UIButton;
+//        btn!.frame = CGRectMake(100, 0, 100, 100);
+////        btn!.setImage(img, forState: UIControlState.Normal);
+//        btn!.addTarget(self, action: "onClick:", forControlEvents: UIControlEvents.TouchUpInside);
+//        
+//        Button1.layer.cornerRadius = 10
+//        self.view.addSubview(Button1)
+//        Button1.layer.borderWidth = 1
+//        Button2.layer.cornerRadius = 10
+//        self.view.addSubview(Button2)
+//        Button2.layer.borderWidth = 1
+//        Button3.layer.cornerRadius = 10
+//        self.view.addSubview(Button3)
+//        Button3.layer.borderWidth = 1
+//        
+//      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
